5e4366974034b869c303793df6ba8b2b1f817be0
Demetrios Kalcassinas <disinas@icloud.com>
2022-01-04 17:25:23 +0200

    40 Password Reset - Requesting a Reset
    When you request a password reset, the back-end of the application generates a random token, then you are emailed a link with that token in the URL, and then when you try to reset the password, you pass it, your email address and new password and the token. Keystone has all the flow for that but it doesn't send email.
    Create RequestReset.js component that calls REQUEST_RESET_MUTATION GraphQL query. At the keystone.ts of the backend

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import { ProductImage } from './schemas/ProductImage';
 import { Product } from './schemas/Product';
 import { User } from './schemas/User';
 import 'dotenv/config';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -25,6 +26,11 @@ const { withAuth } = createAuth({
     fields: ['name', 'email', 'password'],
     // TODO: Add in inital roles here
   },
+  passwordResetLink: {
+    async sendToken(args) {
+      console.log(args);
+    },
+  },
 });
 
 export default withAuth(
@@ -39,7 +45,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO: Add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to the database!');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // Schema items go in here

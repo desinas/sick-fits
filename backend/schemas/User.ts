ea562c80acb823b1d9603cadd10624978cfb639f
Demetrios Kalcassinas <disinas@icloud.com>
2022-01-09 19:50:19 +0200

    54 Creating Order and OrderItem Data Types
    We need to create an order type, as well as order items, in which copy all the fields from the product into an order item. The id is not actually a really good thing to show, so what we can do, is to put a label on this field, so the label field is going to be a virtual field, a virtual field is something that is calculated on demand that's stored in the database, but it's something that is just generated as we need it. Whatever returns this resolver will be what is shown up as the label. In Keystone dashboard we can modify what columns of the Orders schema will be output.

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,21 +1,9 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
-import { permissions, rules } from '../access';
 
 export const User = list({
-  access: {
-    create: () => true,
-    read: rules.canManageUsers,
-    update: rules.canManageUsers,
-    // only people with the permission can delete themselves!
-    // You can't delete yourself
-    delete: permissions.canManageUsers,
-  },
-  ui: {
-    // hide the backend UI from regular users
-    hideCreate: (args) => !permissions.canManageUsers(args),
-    hideDelete: (args) => !permissions.canManageUsers(args),
-  },
+  // access:
+  // ui
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
@@ -29,16 +17,5 @@ export const User = list({
       },
     }),
     orders: relationship({ ref: 'Order.user', many: true }),
-    role: relationship({
-      ref: 'Role.assignedTo',
-      access: {
-        create: permissions.canManageUsers,
-        update: permissions.canManageUsers,
-      },
-    }),
-    products: relationship({
-      ref: 'Product.user',
-      many: true,
-    }),
   },
 });
